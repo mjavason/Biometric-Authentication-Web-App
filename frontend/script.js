2e30e0abb38a6eef09fc5f044f8ca084469dfe06
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-07-10 16:41:57 +0100

    Update script

--- frontend/script.js
+++ frontend/script.js
@@ -167,14 +167,12 @@ async function getCredential(email) {
       challenge: Uint8Array.from(challenge, (c) => c.charCodeAt(0)),
       allowCredentials: [
         {
-          id: Uint8Array.from(user.credentials.credentialId, (c) =>
-            c.charCodeAt(0)
-          ),
+          id: Uint8Array.from(user.id, (c) => c.charCodeAt(0)),
           type: 'public-key',
-          userVerification: 'required',
           // transports: ['usb', 'ble', 'nfc'],
         },
       ],
+      userVerification: 'required',
       timeout: 60000,
     };
 
